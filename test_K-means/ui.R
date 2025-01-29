3c755093189c1b49c0f39e7ed3c7ae594dfacc4d
Asanned <95966995+Asanned@users.noreply.github.com>
2025-01-29 16:54:43 +0100

    Initial commit

---
+++ test_K-means/ui.R
@@ -0,0 +1,97 @@
+shinyUI(fluidPage(
+  includeCSS('www/styles.css'),
+  shinyjs::useShinyjs(),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput('nbNodes', 'Number of nodes', 100, min = 10),
+      numericInput('nbClusters', 'Number of clusters', 4, min = 1, max = 9),
+
+      selectInput('distanceType', 'distance', choices = c('Euclidean', 'Manhattan'), selected = 'Euclidean'),
+
+      div(class = 'dataset-info',
+        div(
+          actionButton('showDatasetModal', 'Select a dataset to visualize'),
+          class = 'div1'
+        ), div(
+          textOutput('currentDatafile'),
+          class = 'div2 req-df'
+        ), div(
+          textOutput('currentDataframe'),
+          class = 'div3 req-df'
+        ), div(
+          selectInput('colX', 'Select the x-axis', choices = NULL),
+          class = 'div4 req-df'
+        ), div(
+          selectInput('colY', 'Select the y-axis', choices = NULL),
+          class = 'div5 req-df'
+        ), div(
+          checkboxInput('useLabels', 'Use custom labels'),
+          class = 'div6 req-df'
+        ), div(
+          conditionalPanel(
+            condition = "input.useLabels",
+            selectInput('colLabel', 'Column to use as labels', choices = NULL)
+          ),
+          class = 'div7 req-df'
+        ),
+      ),
+
+      br(),
+      actionButton('reset', 'Reset', icon = icon('arrows-rotate')),
+      actionButton('step', 'Step', icon = icon('arrow-right')),
+
+      br(),
+      hr(),
+
+      checkboxInput('placeInitClusters', 'Manually place initial clusters'),
+      actionButton('defaultManualClusters', 'Set manual clusters to default'),
+      br(),
+      conditionalPanel("input.placeInitClusters",
+        div(id = 'cluster-container',
+            lapply(1:9, function(i) {
+              div(class = 'mc-grid-parent cluster',
+                  div(paste0('Cluster n° ', i), class = 'mc-text'),
+                  div(numericInput(
+                    paste0('manualCluster', i, 'x'),
+                    'x',
+                    value = 0.5,
+                    min = 0,
+                    max = 1,
+                    step = 0.1
+                  ), class = 'mc-x'),
+                  div(numericInput(
+                    paste0('manualCluster', i, 'y'),
+                    'y',
+                    value = 0.5,
+                    min = 0,
+                    max = 1,
+                    step = 0.1
+                  ), class = 'mc-y')
+              )
+            })
+        )
+      )
+    ),
+    mainPanel(
+      div(
+        plotOutput('kMeans'),
+        class = 'plot'
+      )
+    )
+  ),
+
+  tags$script(HTML(
+    '
+    $(document).on("shiny:connected", function(e) {
+      Shiny.onInputChange("innerWidth", window.innerWidth);
+    });
+
+    $(window).resize(function(e) {
+      Shiny.onInputChange("innerWidth", window.innerWidth);
+    });
+    '
+  ))
+))
+
+
+
